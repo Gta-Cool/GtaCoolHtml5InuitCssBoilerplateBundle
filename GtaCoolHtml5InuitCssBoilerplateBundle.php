dd9c735258508989bad4c6d6b169083bc7899000
Gta-Cool <jplugaru@hotmail.fr>
2013-10-02 19:46:11 +0200

    Generate bundle structure

---
+++ GtaCoolHtml5InuitCssBoilerplateBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GtaCool\Bundle\Html5InuitCssBoilerplateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GtaCoolHtml5InuitCssBoilerplateBundle extends Bundle
+{
+}
