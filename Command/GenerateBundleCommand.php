c9bd985eaaee7adc8f5e59e60ed58b7e563cf678
Gta-Cool <jplugaru@hotmail.fr>
2013-10-03 16:05:00 +0200

    Update command to generate bundle with boilerplate twig and inuit.css inside

--- Command/GenerateBundleCommand.php
+++ Command/GenerateBundleCommand.php
@@ -4,8 +4,6 @@ namespace GtaCool\Bundle\Html5InuitCssBoilerplateBundle\Command;
 
 use Sensio\Bundle\GeneratorBundle\Command\GenerateBundleCommand as BaseGenerateBundleCommand;
 use GtaCool\Bundle\Html5InuitCssBoilerplateBundle\Generator\BundleGenerator;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Generates bundles with base template inside
@@ -20,16 +18,19 @@ class GenerateBundleCommand extends BaseGenerateBundleCommand
     protected function configure()
     {
         parent::configure();
-        $this->setName('gta-cool:html5-inuit.css:generate-bundle');
+        $this->setName('gta-cool:boilerplate:generate-bundle');
         $this->setDescription('Generates a bundle with inuit.css and html5 boilerplate installed inside');
         $help = $this->getHelp();
-        $help = str_replace('generate:bundle', 'gta-cool:html5-inuit.css:generate-bundle', $help);
+        $help = str_replace('generate:bundle', 'gta-cool:boilerplate:generate-bundle', $help);
         $help = str_replace('bundles', 'bundles with inuit.css and html5 boilerplate installed inside', $help);
         $this->setHelp($help);
     }
 
     protected function createGenerator()
     {
-        return new BundleGenerator($this->getContainer()->get('filesystem'), __DIR__.'/../Resources/skeleton');
+        return new BundleGenerator(
+            $this->getContainer()->get('filesystem'),
+            $this->getContainer()->getParameter('inuit_css.bundle.resources_installation_dir')
+        );
     }
 }
--- Generator/BundleGenerator.php
+++ Generator/BundleGenerator.php
@@ -2,7 +2,7 @@
 
 namespace GtaCool\Bundle\Html5InuitCssBoilerplateBundle\Generator;
 
-use Sensio\Bundle\GeneratorBundle\Generator\BundleGenerator as BaseBundleGenerator;
+use GtaCool\Bundle\InuitCssBundle\Generator\BundleGenerator as BaseBundleGenerator;
 use Symfony\Component\Filesystem\Filesystem;
 
 /**
@@ -26,18 +26,21 @@ class BundleGenerator extends BaseBundleGenerator
      * Constructor
      *
      * @param Filesystem $filesystem
-     * @param string $skeletonDir
+     * @param string $resourcesInstallationDir
      *
      * @throws \InvalidArgumentException
      */
-    public function __construct(Filesystem $filesystem, $skeletonDir)
+    public function __construct(Filesystem $filesystem, $resourcesInstallationDir)
     {
-        if (!is_string($skeletonDir)) {
-            throw new \InvalidArgumentException("\"$skeletonDir\" must be a string");
+        if (!is_string($resourcesInstallationDir)) {
+            throw new \InvalidArgumentException("\"$resourcesInstallationDir\" must be a string");
         }
 
+        // Need call parent constructor
+        parent::__construct($filesystem, $resourcesInstallationDir);
+
         $this->filesystem = $filesystem;
-        $this->skeletonDir = $skeletonDir;
+        $this->skeletonDir = __DIR__.'/../Resources/skeleton';
     }
 
     public function generate($namespace, $bundle, $dir, $format, $structure)
